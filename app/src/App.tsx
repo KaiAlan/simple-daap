a485f2cfe5f07b280edf10bb1788b1f1bf7a6141
Kaialan <satyajit1501@gmail.com>
2024-01-21 21:55:36 +0530

    first commit - simple setup

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <main className="flex justify-center items-center min-w-screen min-h-screen w-full h-screen">
+      <h1>Hello from vite app</h1>
+    </main>
+  )
+}
+
+export default App
